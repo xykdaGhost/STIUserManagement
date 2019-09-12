4970125f9139f785000e5ddd7695e79995816ed8
xykdaGhost <yikaixiong@gmail.com>
2019-09-12 16:47:29 +0900

    phone

--- pages/apply/apply.js
+++ pages/apply/apply.js
@@ -5,7 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    show: false,//控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ['电气学院', '光电学院', '自动化学院', '电信学院', '西边其他学院', '东边其他学院'],//下拉列表的数据
+    index: 0//选择的下拉列表下标
+  },
+  // 点击下拉显示框
+  selectTap() {
+    this.setData({
+      show: !this.data.show
+    });
+  },
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+    this.setData({
+      index: Index,
+      show: !this.data.show
+    });
   },
 
   /**
